02d742b7fe2f07d8a8ff649dbf559190bf3472f1
AhmadSafdar <azs0202@auburn.edu>
2018-01-19 23:05:01 -0600

    Change name

--- JavaHelloWorldApp/src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ JavaHelloWorldApp/src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Ahmad Safdar   123!");
+        response.getWriter().print("Ahmad Safdar");
     }
 
 }
